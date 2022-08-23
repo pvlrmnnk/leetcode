a14bdebc064d9d9a1ab605140a9e3501d927f213
Pavlo Romanenko <mail@pvlrmnnk.com>
2022-08-23 14:11:05 +0300

    Resolve #876 Middle of the Linked List

---
+++ p0876/solution.go
@@ -0,0 +1,22 @@
+package p0876
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type MiddleOfTheLinkedListSolution func(head *ListNode) *ListNode
+
+func Solution() MiddleOfTheLinkedListSolution {
+	return func(head *ListNode) *ListNode {
+		fast := head
+		slow := head
+
+		for fast != nil && fast.Next != nil {
+			fast = fast.Next.Next
+			slow = slow.Next
+		}
+
+		return slow
+	}
+}
---
+++ p0876/solution_test.go
@@ -0,0 +1,41 @@
+package p0876
+
+import (
+	"testing"
+
+	"github.com/pvlrmnnk/leetcode-go/common/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMergeTwoSortedListsSolution(t *testing.T) {
+	ss := []MiddleOfTheLinkedListSolution{
+		Solution(),
+	}
+
+	for i, s := range ss {
+		t.Run(test.N("s", i), func(t *testing.T) {
+			tcs := []struct {
+				head, r *ListNode
+			}{
+				func() struct{ head, r *ListNode } {
+					head := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}
+					return struct{ head, r *ListNode }{head, head.Next.Next}
+				}(),
+				func() struct{ head, r *ListNode } {
+					head := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, &ListNode{6, nil}}}}}}
+					return struct{ head, r *ListNode }{head, head.Next.Next.Next}
+				}(),
+				{
+					nil, nil,
+				},
+			}
+
+			for i, tc := range tcs {
+				t.Run(test.N("tc", i), func(t *testing.T) {
+					r := s(tc.head)
+					assert.EqualValues(t, tc.r, r)
+				})
+			}
+		})
+	}
+}
