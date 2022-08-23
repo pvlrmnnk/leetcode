c34b591a069fbb4c0b9d5929c81a7a19c7d80976
Pavlo Romanenko <mail@pvlrmnnk.com>
2022-08-23 14:23:23 +0300

    Update #142 test

--- p0142/solution_test.go
+++ p0142/solution_test.go
@@ -16,7 +16,24 @@ func TestMergeTwoSortedListsSolution(t *testing.T) {
 		t.Run(test.N("s", i), func(t *testing.T) {
 			tcs := []struct {
 				head, r *ListNode
-			}{}
+			}{
+				func() struct{ head, r *ListNode } {
+					head := &ListNode{3, &ListNode{2, &ListNode{0, &ListNode{-4, nil}}}}
+					head.Next.Next.Next.Next = head.Next
+					return struct{ head, r *ListNode }{head, head.Next}
+				}(),
+				func() struct{ head, r *ListNode } {
+					head := &ListNode{1, &ListNode{2, nil}}
+					head.Next.Next = head
+					return struct{ head, r *ListNode }{head, head}
+				}(),
+				{
+					&ListNode{1, nil}, nil,
+				},
+				{
+					nil, nil,
+				},
+			}
 
 			for i, tc := range tcs {
 				t.Run(test.N("tc", i), func(t *testing.T) {
