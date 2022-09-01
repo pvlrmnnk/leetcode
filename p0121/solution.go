452b4ed6feb2745b448ac797800bf429e8947611
Pavlo Romanenko <mail@pvlrmnnk.com>
2022-09-01 20:01:27 +0300

    Linter refactoring for #121

--- p0121/solution.go
+++ p0121/solution.go
@@ -2,40 +2,36 @@ package p0121
 
 type BestTimeToBuyAndSellStockSolution func(prices []int) int
 
-func DumbSolution() BestTimeToBuyAndSellStockSolution {
-	return func(prices []int) int {
-		profit := 0
+func DumbSolution(prices []int) int {
+	profit := 0
 
-		for i := 0; i < len(prices)-1; i++ {
-			for j := i + 1; j < len(prices); j++ {
-				possibleProfit := prices[j] - prices[i]
-				if possibleProfit > profit {
-					profit = possibleProfit
-				}
+	for i := 0; i < len(prices)-1; i++ {
+		for j := i + 1; j < len(prices); j++ {
+			possibleProfit := prices[j] - prices[i]
+			if possibleProfit > profit {
+				profit = possibleProfit
 			}
 		}
-
-		return profit
 	}
+
+	return profit
 }
 
-func SmartSolution() BestTimeToBuyAndSellStockSolution {
-	return func(prices []int) int {
-		left, right := 0, 1
-		maxProfit, profit := 0, 0
+func SmartSolution(prices []int) int {
+	left, right := 0, 1
+	maxProfit, profit := 0, 0
 
-		for right < len(prices) {
-			if prices[left] < prices[right] {
-				profit = prices[right] - prices[left]
-				if profit > maxProfit {
-					maxProfit = profit
-				}
-			} else {
-				left = right
+	for right < len(prices) {
+		if prices[left] < prices[right] {
+			profit = prices[right] - prices[left]
+			if profit > maxProfit {
+				maxProfit = profit
 			}
-			right++
+		} else {
+			left = right
 		}
-
-		return maxProfit
+		right++
 	}
+
+	return maxProfit
 }
--- p0121/solution_test.go
+++ p0121/solution_test.go
@@ -3,30 +3,51 @@ package p0121
 import (
 	"testing"
 
-	"github.com/pvlrmnnk/leetcode-go/common/test"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestBestTimeToBuyAndSellStockSolution(t *testing.T) {
-	ss := []BestTimeToBuyAndSellStockSolution{
-		DumbSolution(),
-		SmartSolution(),
+	t.Parallel()
+
+	ss := []struct {
+		name string
+		fn   BestTimeToBuyAndSellStockSolution
+	}{
+		{"dumb solution", DumbSolution},
+		{"smart solution", SmartSolution},
 	}
 
-	for i, s := range ss {
-		t.Run(test.N("s", i), func(t *testing.T) {
-			tcs := []struct {
+	for _, s := range ss {
+		s := s
+		t.Run(s.name, func(t *testing.T) {
+			t.Parallel()
+
+			type args struct {
 				prices []int
-				r      int
+			}
+			tcs := []struct {
+				name string
+				args args
+				want int
 			}{
-				{[]int{7, 1, 5, 3, 6, 4}, 5},
-				{[]int{7, 6, 4, 3, 1}, 0},
+				{
+					"",
+					args{[]int{7, 1, 5, 3, 6, 4}},
+					5,
+				},
+				{
+					"",
+					args{[]int{7, 6, 4, 3, 1}},
+					0,
+				},
 			}
 
-			for i, tc := range tcs {
-				t.Run(test.N("tc", i), func(t *testing.T) {
-					r := s(tc.prices)
-					assert.Equal(t, tc.r, r)
+			for _, tc := range tcs {
+				t.Run(tc.name, func(t *testing.T) {
+					t.Parallel()
+
+					got := s.fn(tc.args.prices)
+					assert.Equal(t, tc.want, got)
 				})
 			}
 		})
