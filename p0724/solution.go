5a38bac8d0d2ea5bb45e19a406b55624971bdc4e
Pavlo Romanenko <mail@pvlrmnnk.com>
2022-09-02 15:20:22 +0300

    Linter refactoring for #724

--- p0724/solution.go
+++ p0724/solution.go
@@ -2,22 +2,20 @@ package p0724
 
 type PivotIndexSolution func(nums []int) int
 
-func Solution() PivotIndexSolution {
-	return func(nums []int) int {
-		var acc1, acc2 int
+func Solution(nums []int) int {
+	var acc1, acc2 int
 
-		for _, i := range nums {
-			acc1 += i
-		}
-
-		for idx, i := range nums {
-			acc1 -= i
-			if acc1 == acc2 {
-				return idx
-			}
-			acc2 += i
-		}
-
-		return -1
+	for _, num := range nums {
+		acc1 += num
 	}
+
+	for idx, num := range nums {
+		acc1 -= num
+		if acc1 == acc2 {
+			return idx
+		}
+		acc2 += num
+	}
+
+	return -1
 }
--- p0724/solution_test.go
+++ p0724/solution_test.go
@@ -3,31 +3,61 @@ package p0724
 import (
 	"testing"
 
-	"github.com/pvlrmnnk/leetcode-go/common/test"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPivotIndexSolution(t *testing.T) {
-	solutions := []PivotIndexSolution{
-		Solution(),
+	t.Parallel()
+
+	ss := []struct {
+		name string
+		fn   PivotIndexSolution
+	}{
+		{"solution", Solution},
 	}
 
-	for i, solution := range solutions {
-		t.Run(test.Solution(i), func(t *testing.T) {
-			tcs := []struct {
+	for _, s := range ss {
+		s := s
+		t.Run(s.name, func(t *testing.T) {
+			t.Parallel()
+
+			type args struct {
 				nums []int
-				r    int
+			}
+			tcs := []struct {
+				name string
+				args args
+				want int
 			}{
-				{[]int{1, 7, 3, 6, 5, 6}, 3},
-				{[]int{1, 2, 3}, -1},
-				{[]int{2, 1, -1}, 0},
-				{[]int{-1, -1, -1, 0, 1, 1}, 0},
+				{
+					"",
+					args{[]int{1, 7, 3, 6, 5, 6}},
+					3,
+				},
+				{
+					"",
+					args{[]int{1, 2, 3}},
+					-1,
+				},
+				{
+					"",
+					args{[]int{2, 1, -1}},
+					0,
+				},
+				{
+					"",
+					args{[]int{-1, -1, -1, 0, 1, 1}},
+					0,
+				},
 			}
 
-			for i, tc := range tcs {
-				t.Run(test.TestCase(i), func(t *testing.T) {
-					r := solution(tc.nums)
-					assert.Equal(t, tc.r, r)
+			for _, tc := range tcs {
+				tc := tc
+				t.Run(tc.name, func(t *testing.T) {
+					t.Parallel()
+
+					got := s.fn(tc.args.nums)
+					assert.Equal(t, tc.want, got)
 				})
 			}
 		})
