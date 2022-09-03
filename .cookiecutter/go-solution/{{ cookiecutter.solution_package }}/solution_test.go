06613a88c4640e6fa525bd54b21e7a7c65840d76
Pavlo Romanenko <mail@pvlrmnnk.com>
2022-09-03 23:25:57 +0300

    Update go solution template test

--- .cookiecutter/go-solution/{{ cookiecutter.solution_package }}/solution_test.go
+++ .cookiecutter/go-solution/{{ cookiecutter.solution_package }}/solution_test.go
@@ -7,27 +7,42 @@ import (
 )
 
 func Test{{ cookiecutter.solution_fn_type }}(t *testing.T) {
-	solutions := []struct {
+	t.Parallel()
+
+	ss := []struct {
 		name string
 		fn   {{ cookiecutter.solution_fn_type }}
 	}{
 		{"solution", Solution},
 	}
 
-	for _, solution := range solutions {
-		t.Run(solution.name, func(t *testing.T) {
-			testCases := []struct {
+	for _, s := range ss {
+		s := s
+		t.Run(s.name, func(t *testing.T) {
+			t.Parallel()
+
+			type args struct {
+				//
+			}
+			tcs := []struct {
 				name string
+				args args
+				want any
 			}{
 				{
 					"",
+					args{},
+					nil,
 				},
 			}
 
-			for _, testCase := range testCases {
-				t.Run(testCase.name, func(t *testing.T) {
-					//_ := solution.fn()
-					assert.True(t, true)
+			for _, tc := range tcs {
+				tc := tc
+				t.Run(tc.name, func(t *testing.T) {
+					t.Parallel()
+
+					got := s.fn(tc.args)
+					assert.Equal(t, tc.want, got)
 				})
 			}
 		})
