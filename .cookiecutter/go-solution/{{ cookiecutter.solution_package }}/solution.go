d3c5c1085c67ca0ddddf506327fcc201747a6762
Pavlo Romanenko <mail@pvlrmnnk.com>
2022-09-09 20:06:26 +0300

    Update go solution template

--- .cookiecutter/go-solution/{{ cookiecutter.solution_package }}/solution.go
+++ .cookiecutter/go-solution/{{ cookiecutter.solution_package }}/solution.go
@@ -2,6 +2,8 @@ package {{ cookiecutter.solution_package }}
 
 type {{ cookiecutter.solution_fn_type }} func{{ cookiecutter.solution_fn_signature }}
 
+// Time complexity: O()
+// Space complexity: O()
 func Solution{{ cookiecutter.solution_fn_signature }} {
 	panic("not_implemented")
 }
